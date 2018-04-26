c6fd828b01ceffae875c6af33cf482c13939e17f
Valentin Bourdaud <bourdaudv@gmail.com>
2018-04-26 12:14:08 +0200

    Nettoyage classe exo monotache

--- src/main/java/monotache/noncorrige/AppFacturation.java
+++ src/main/java/monotache/noncorrige/AppFacturation.java
@@ -1,10 +1,12 @@
 package monotache.noncorrige;
 
+import monotache.corrige.DateUtils;
+
 public class AppFacturation {
 
 	public static void main(String[] args) {
 		Facturation facturation = new Facturation();
-		Facture facture = facturation.creerFacture(18.5, "18/11/2017");
+		Facture facture = facturation.creerFacture(18.5, DateUtils.parse("18/11/2017"));
 
 	}
 
